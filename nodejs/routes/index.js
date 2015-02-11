74dc160847d6437a02e5b9b9b5b5c91d14a5e430
makuta <279495889@qq.com>
2015-02-11 21:27:42 +0800

    update

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -103,7 +103,7 @@ module.exports = function(app) {
 			var ts = createTimeStamp();
 			var nonceStr = createNonceStr();
 			var ticket = resp.ticket;
-			var signature = sign(ticket, url);
+			var signature = sign(ticket, nonceStr, ts, url);
 			var signature = calcSignature(ticket, nonceStr, ts, url);
 
 			cachedSignatures[url] = {
